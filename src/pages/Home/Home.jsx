17a44787b1a79011ed4900639506a9d2a830fbe0
deepak kumar <deepak7292832956@gmail.com>
2026-02-13 07:57:19 +0530

    worked on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './home.css';
-import Navbar from '../../components/Navbar/Navbar';
+import React from "react";
+import "./home.css";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import Navbar from "../../components/Navbar/Navbar";
 const Home = () => {
   return (
-    <div className='home'>
-        <Navbar/>
-    </div>
-  )
-}
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="title" className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order. A young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="play-icon" />
+              Play
+            </button>
 
-export default Home
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="info-icon" />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
